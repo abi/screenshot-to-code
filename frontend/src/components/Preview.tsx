3f071e7a68d735dac462c4ccf067c17242ea712e
Abi Raja <abimanyuraja@gmail.com>
2023-11-14 18:54:05 -0500

    make preview look better

--- frontend/src/components/Preview.tsx
+++ frontend/src/components/Preview.tsx
@@ -8,7 +8,7 @@ function Preview({ code }: Props) {
       <iframe
         title="Iframe Example"
         srcDoc={code}
-        className="border-[5px] border-black rounded-[33px] p-4 shadow-lg 
+        className="border-[4px] border-black rounded-[20px] shadow-lg 
         transform scale-[0.8] origin-top-left w-[1280px] h-[832px]"
       ></iframe>
     </div>
