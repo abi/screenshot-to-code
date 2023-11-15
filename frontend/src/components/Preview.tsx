99508d8e03863dbf7e969edd004f085db5b8e93e
Abi Raja <abimanyuraja@gmail.com>
2023-11-15 09:51:34 -0500

    throttle the iframe preview so that it doesn't stop updating at the end of code generation due to too many updates

--- frontend/src/components/Preview.tsx
+++ frontend/src/components/Preview.tsx
@@ -1,13 +1,17 @@
+import useThrottle from "../hooks/useThrottle";
+
 interface Props {
   code: string;
 }
 
 function Preview({ code }: Props) {
+  const throttledCode = useThrottle(code, 200);
+
   return (
     <div className="w-[704px]">
       <iframe
-        title="Iframe Example"
-        srcDoc={code}
+        title="Preview"
+        srcDoc={throttledCode}
         className="border-[4px] border-black rounded-[20px] shadow-lg 
         transform scale-[0.8] origin-top-left w-[1280px] h-[832px]"
       ></iframe>
