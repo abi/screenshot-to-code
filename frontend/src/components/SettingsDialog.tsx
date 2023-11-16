e27c278a797010256dc1c096b09d506a661d3f5f
Abi Raja <abimanyuraja@gmail.com>
2023-11-16 17:41:24 -0500

    update copy for Placeholder Image Generation setting

--- frontend/src/components/SettingsDialog.tsx
+++ frontend/src/components/SettingsDialog.tsx
@@ -25,7 +25,12 @@ function SettingsDialog({ settings, setSettings }: Props) {
         <DialogHeader>
           <DialogTitle className="mb-4">Settings</DialogTitle>
           <div className="flex items-center space-x-2">
-            <Label htmlFor="image-generation">Image Generation</Label>
+            <Label htmlFor="image-generation">
+              <div>DALL-E Placeholder Image Generation</div>
+              <div className="font-light mt-2">
+                More fun with it but if you want to save money, turn it off.
+              </div>
+            </Label>
             <Switch
               id="image-generation"
               checked={settings.isImageGenerationEnabled}
