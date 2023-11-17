02ddd63db53a66b6e9219c3159a5b2895c083df2
Abi Raja <abimanyuraja@gmail.com>
2023-11-16 19:12:41 -0500

    set up vite base URL for when it's deployed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.VITE_IS_DEPLOYED ? "/free-tools/screenshot-to-code/" : "",
   plugins: [react(), checker({ typescript: true })],
   resolve: {
     alias: {
