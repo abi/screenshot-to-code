103d1ce12ce56c273ae531d4e1dfd80ef3a62144
Abi Raja <abimanyuraja@gmail.com>
2023-11-20 11:50:06 -0500

    centralize config in config.ts

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,5 +2,8 @@
 export const IS_RUNNING_ON_CLOUD =
   import.meta.env.VITE_IS_DEPLOYED === "true" || false;
 
+export const WS_BACKEND_URL =
+  import.meta.env.VITE_WS_BACKEND_URL || "ws://127.0.0.1:7001";
+
 export const HTTP_BACKEND_URL =
   import.meta.env.VITE_HTTP_BACKEND_URL || "http://127.0.0.1:7001";
