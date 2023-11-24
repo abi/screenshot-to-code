6a06d890aa130811ead5a6d2fa36e285fff3cd2e
clean99 <xff9924@gmail.com>
2023-11-24 11:10:51 +0800

    feat: update instructions convert

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,12 @@
+import { Instruction } from "@/types";
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
  
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const handleInstructions = (instructions: string): string =>
+  (JSON.parse(instructions) || []).map((line: Instruction) => 
+    `- ${line.element} in the ${line.location} has a mistake: ${line.mistake}, please update: ${line.improvement}.`).join("\n\n");
+
