e14760718e555af181243b8ab73d9b840b73b792
Abi Raja <abimanyuraja@gmail.com>
2023-11-26 14:54:02 -0500

    collect emails on hosted version

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -7,3 +7,6 @@ export const WS_BACKEND_URL =
 
 export const HTTP_BACKEND_URL =
   import.meta.env.VITE_HTTP_BACKEND_URL || "http://127.0.0.1:7001";
+
+export const PICO_BACKEND_FORM_SECRET =
+  import.meta.env.VITE_PICO_BACKEND_FORM_SECRET || null;
