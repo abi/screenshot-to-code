13a19cdea2b726a1ad81db30480a28767353d268
Abi Raja <abimanyuraja@gmail.com>
2023-11-27 17:39:05 -0500

    inject code into <head> from env var for use in hosted version

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,29 @@
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import checker from "vite-plugin-checker";
 import react from "@vitejs/plugin-react";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: process.env.VITE_IS_DEPLOYED ? "/free-tools/screenshot-to-code/" : "",
-  plugins: [react(), checker({ typescript: true })],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    base: process.env.VITE_IS_DEPLOYED ? "/free-tools/screenshot-to-code/" : "",
+    plugins: [
+      react(),
+      checker({ typescript: true }),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            injectHead: process.env.VITE_INJECT_HEAD,
+          },
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-});
+  });
+};
