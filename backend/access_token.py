13888db9b57313f4ff5cd87bfdbaf61b15ba42f7
Abi Raja <abimanyuraja@gmail.com>
2023-11-29 14:28:36 -0500

    update URL to prod URL

--- backend/access_token.py
+++ backend/access_token.py
@@ -5,7 +5,9 @@ import httpx
 
 async def validate_access_token(access_code: str):
     async with httpx.AsyncClient() as client:
-        url = "http://localhost:8000/screenshot_to_code/validate_access_token"
+        url = (
+            "https://backend.buildpicoapps.com/screenshot_to_code/validate_access_token"
+        )
         data = json.dumps(
             {
                 "access_code": access_code,
