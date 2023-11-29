14dbca8c0748d17f28639353025bcd0d0f205f78
kachbit <kacherpearce@gmail.com>
2023-11-28 17:53:50 -0800

    fix conflict

--- frontend/src/components/ui/select.tsx
+++ frontend/src/components/ui/select.tsx
@@ -88,6 +88,10 @@ const SelectContent = React.forwardRef<
     >
       <SelectScrollUpButton />
       <SelectPrimitive.Viewport
+        className={cn(
+          "p-1",
+          position === "popper" &&
+            "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]"
         )}
       >
         {children}
