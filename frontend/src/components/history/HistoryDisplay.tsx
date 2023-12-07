b3b478d6d1739f83ea01452e80105faa8a7eac43
Abi Raja <abimanyuraja@gmail.com>
2023-12-07 13:21:10 -0500

    add hover card to show full prompt

--- frontend/src/components/history/HistoryDisplay.tsx
+++ frontend/src/components/history/HistoryDisplay.tsx
@@ -1,7 +1,11 @@
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { History, HistoryItemType } from "./history_types";
+import { History } from "./history_types";
 import toast from "react-hot-toast";
 import classNames from "classnames";
+import {
+  HoverCard,
+  HoverCardTrigger,
+  HoverCardContent,
+} from "../ui/hover-card";
 
 interface Props {
   history: History;
@@ -10,17 +14,17 @@ interface Props {
   shouldDisableReverts: boolean;
 }
 
-function displayHistoryItemType(itemType: HistoryItemType) {
-  switch (itemType) {
-    case "ai_create":
-      return "Create";
-    case "ai_edit":
-      return "Edit";
-    default:
-      // TODO: Error out since this is exhaustive
-      return "Unknown";
-  }
-}
+// function displayHistoryItemType(itemType: HistoryItemType) {
+//   switch (itemType) {
+//     case "ai_create":
+//       return "Create";
+//     case "ai_edit":
+//       return "Edit";
+//     default:
+//       // TODO: Error out since this is exhaustive
+//       return "Unknown";
+//   }
+// }
 
 export default function HistoryDisplay({
   history,
@@ -31,45 +35,50 @@ export default function HistoryDisplay({
   return history.length === 0 ? null : (
     <div className="flex flex-col h-screen">
       <h1 className="font-bold mb-2">Versions</h1>
-      <ScrollArea className="flex-1 overflow-y-auto">
-        <ul className="space-y-0 flex flex-col-reverse">
-          {history.map((item, index) => (
-            <li
-              key={index}
-              className={classNames(
-                "flex items-center space-x-2 justify-between p-2",
-                "border-b cursor-pointer",
-                {
-                  " hover:bg-black hover:text-white": index !== currentVersion,
-                  "bg-slate-500 text-white": index === currentVersion,
+      <ul className="space-y-0 flex flex-col-reverse">
+        {history.map((item, index) => (
+          <li key={index}>
+            <HoverCard>
+              <HoverCardTrigger
+                className={classNames(
+                  "flex items-center justify-between space-x-2 p-2",
+                  "border-b cursor-pointer",
+                  {
+                    " hover:bg-black hover:text-white":
+                      index !== currentVersion,
+                    "bg-slate-500 text-white": index === currentVersion,
+                  }
+                )}
+                onClick={() =>
+                  shouldDisableReverts
+                    ? toast.error(
+                        "Please wait for code generation to complete before viewing an older version."
+                      )
+                    : revertToVersion(index)
                 }
-              )}
-              onClick={() =>
-                shouldDisableReverts
-                  ? toast.error(
-                      "Please wait for code generation to complete before viewing an older version."
-                    )
-                  : revertToVersion(index)
-              }
-            >
-              <div className="flex gap-x-1">
-                <h2 className="text-sm">{displayHistoryItemType(item.type)}</h2>
-                {item.parentIndex !== null && item.parentIndex !== index - 1 ? (
-                  <h2 className="text-sm">
-                    (parent: v{(item.parentIndex || 0) + 1})
+              >
+                {" "}
+                <div className="flex gap-x-1 truncate">
+                  <h2 className="text-sm truncate">
+                    {item.type === "ai_edit" ? item.inputs.prompt : "Create"}
                   </h2>
-                ) : null}
-              </div>
-              <h2 className="text-sm">
-                {item.type === "ai_edit"
-                  ? item.inputs.prompt
-                  : item.inputs.image_url}
-              </h2>
-              <h2 className="text-sm">v{index + 1}</h2>
-            </li>
-          ))}
-        </ul>
-      </ScrollArea>
+                  {/* <h2 className="text-sm">{displayHistoryItemType(item.type)}</h2> */}
+                  {item.parentIndex !== null &&
+                  item.parentIndex !== index - 1 ? (
+                    <h2 className="text-sm">
+                      (parent: v{(item.parentIndex || 0) + 1})
+                    </h2>
+                  ) : null}
+                </div>
+                <h2 className="text-sm">v{index + 1}</h2>
+              </HoverCardTrigger>
+              <HoverCardContent>
+                {item.type === "ai_edit" ? item.inputs.prompt : "Create"}
+              </HoverCardContent>
+            </HoverCard>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
