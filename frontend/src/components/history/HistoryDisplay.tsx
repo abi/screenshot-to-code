a615f25fd73a05da56bfa7eeb438e8dcc3e095c5
Abi Raja <abimanyuraja@gmail.com>
2023-12-13 12:20:44 -0500

    improve history UX

--- frontend/src/components/history/HistoryDisplay.tsx
+++ frontend/src/components/history/HistoryDisplay.tsx
@@ -1,13 +1,16 @@
 import { History } from "./history_types";
 import toast from "react-hot-toast";
 import classNames from "classnames";
-import {
-  HoverCard,
-  HoverCardTrigger,
-  HoverCardContent,
-} from "../ui/hover-card";
+
 import { Badge } from "../ui/badge";
 import { renderHistory } from "./utils";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "../ui/collapsible";
+import { Button } from "../ui/button";
+import { CaretSortIcon } from "@radix-ui/react-icons";
 
 interface Props {
   history: History;
@@ -30,38 +33,51 @@ export default function HistoryDisplay({
       <ul className="space-y-0 flex flex-col-reverse">
         {renderedHistory.map((item, index) => (
           <li key={index}>
-            <HoverCard>
-              <HoverCardTrigger
+            <Collapsible>
+              <div
                 className={classNames(
-                  "flex items-center justify-between space-x-2 p-2",
+                  "flex items-center justify-between space-x-2 w-full pr-2",
                   "border-b cursor-pointer",
                   {
                     " hover:bg-black hover:text-white": !item.isActive,
                     "bg-slate-500 text-white": item.isActive,
                   }
                 )}
-                onClick={() =>
-                  shouldDisableReverts
-                    ? toast.error(
-                        "Please wait for code generation to complete before viewing an older version."
-                      )
-                    : revertToVersion(index)
-                }
               >
-                {" "}
-                <div className="flex gap-x-1 truncate">
-                  <h2 className="text-sm truncate">{item.summary}</h2>
-                  {item.parentVersion !== null && (
-                    <h2 className="text-sm">(parent: {item.parentVersion})</h2>
-                  )}
+                <div
+                  className="flex justify-between truncate flex-1 p-2"
+                  onClick={() =>
+                    shouldDisableReverts
+                      ? toast.error(
+                          "Please wait for code generation to complete before viewing an older version."
+                        )
+                      : revertToVersion(index)
+                  }
+                >
+                  <div className="flex gap-x-1 truncate">
+                    <h2 className="text-sm truncate">{item.summary}</h2>
+                    {item.parentVersion !== null && (
+                      <h2 className="text-sm">
+                        (parent: {item.parentVersion})
+                      </h2>
+                    )}
+                  </div>
+                  <h2 className="text-sm">v{index + 1}</h2>
                 </div>
-                <h2 className="text-sm">v{index + 1}</h2>
-              </HoverCardTrigger>
-              <HoverCardContent>
-                <div>{item.summary}</div>
-                <Badge>{item.type}</Badge>
-              </HoverCardContent>
-            </HoverCard>
+                <CollapsibleTrigger asChild>
+                  <Button variant="ghost" size="sm" className="h-6">
+                    <CaretSortIcon className="h-4 w-4" />
+                    <span className="sr-only">Toggle</span>
+                  </Button>
+                </CollapsibleTrigger>
+              </div>
+              <CollapsibleContent className="w-full bg-slate-300 p-2">
+                <div>Full prompt: {item.summary}</div>
+                <div className="flex justify-end">
+                  <Badge>{item.type}</Badge>
+                </div>
+              </CollapsibleContent>
+            </Collapsible>
           </li>
         ))}
       </ul>
