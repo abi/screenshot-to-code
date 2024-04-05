575847f845357d2b0c885da49fdb0cf81b8aa3c2
Abi Raja <abimanyuraja@gmail.com>
2024-04-05 14:40:51 -0400

    switch to subscribe banner

--- frontend/src/components/OnboardingNote.tsx
+++ frontend/src/components/OnboardingNote.tsx
@@ -6,25 +6,13 @@ export function OnboardingNote() {
   return (
     <div className="flex flex-col space-y-4 bg-green-700 p-2 rounded text-stone-200 text-sm">
       <span>
-        To use Screenshot to Code,{" "}
         <a
           className="inline underline hover:opacity-70 cursor-pointer"
           onClick={() => setPricingDialogOpen(true)}
           target="_blank"
         >
-          subscribe to a paid plan ($15/month) to get access to all supported
-          models
-        </a>{" "}
-        or use your own OpenAI API key with GPT4 vision access (
-        <a
-          href="https://github.com/abi/screenshot-to-code/blob/main/Troubleshooting.md"
-          className="inline underline hover:opacity-70"
-          target="_blank"
-        >
-          follow these instructions to deposit a minimum $5 credit and obtain a
-          key
+          Subscribe to get started
         </a>
-        ).
       </span>
     </div>
   );
