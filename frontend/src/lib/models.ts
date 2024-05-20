08fb31862153a6343c8937011abd525d975a3b1c
Naman <1608naman@gmail.com>
2024-05-20 09:05:03 +0530

    add gemini 1.5 pro to lib models

--- frontend/src/lib/models.ts
+++ frontend/src/lib/models.ts
@@ -5,6 +5,7 @@ export enum CodeGenerationModel {
   GPT_4_TURBO_2024_04_09 = "gpt-4-turbo-2024-04-09",
   GPT_4_VISION = "gpt_4_vision",
   CLAUDE_3_SONNET = "claude_3_sonnet",
+  GEMINI_1_5_PRO = "gemini-1.5-pro",
 }
 
 // Will generate a static error if a model in the enum above is not in the descriptions
@@ -15,4 +16,5 @@ export const CODE_GENERATION_MODEL_DESCRIPTIONS: {
   "gpt-4-turbo-2024-04-09": { name: "GPT-4 Turbo (Apr 2024)", inBeta: false },
   gpt_4_vision: { name: "GPT-4 Vision (Nov 2023)", inBeta: false },
   claude_3_sonnet: { name: "Claude 3 Sonnet", inBeta: false },
+  "gemini-1.5-pro": { name: "Gemini 1.5 Pro", inBeta: true },
 };
