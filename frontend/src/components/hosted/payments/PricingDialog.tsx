54557de762a57332bf9c47a1a904d5520017f340
Abi Raja <abimanyuraja@gmail.com>
2024-07-22 14:47:09 -0400

    remove old non-subscription code

--- frontend/src/components/hosted/payments/PricingDialog.tsx
+++ frontend/src/components/hosted/payments/PricingDialog.tsx
@@ -15,8 +15,6 @@ import { useStore } from "../../../store/store";
 
 const LOGOS = ["microsoft", "amazon", "mit", "stanford", "bytedance", "baidu"];
 
-const SELL_SUBSCRIPTIONS = true;
-
 const PricingDialog: React.FC = () => {
   const { checkout, isLoadingCheckout } = useStripeCheckout();
   const [paymentInterval, setPaymentInterval] = React.useState<
@@ -45,190 +43,117 @@ const PricingDialog: React.FC = () => {
           </DialogTitle>
         </DialogHeader>
 
-        {SELL_SUBSCRIPTIONS && (
-          <div className="flex justify-center gap-x-2 mt-2">
-            <Button
-              variant={paymentInterval === "monthly" ? "default" : "secondary"}
-              onClick={() => setPaymentInterval("monthly")}
-            >
-              Monthly
-            </Button>
-            <Button
-              variant={paymentInterval === "yearly" ? "default" : "secondary"}
-              onClick={() => setPaymentInterval("yearly")}
-            >
-              Yearly (2 months free)
-            </Button>
-          </div>
-        )}
+        <div className="flex justify-center gap-x-2 mt-2">
+          <Button
+            variant={paymentInterval === "monthly" ? "default" : "secondary"}
+            onClick={() => setPaymentInterval("monthly")}
+          >
+            Monthly
+          </Button>
+          <Button
+            variant={paymentInterval === "yearly" ? "default" : "secondary"}
+            onClick={() => setPaymentInterval("yearly")}
+          >
+            Yearly (2 months free)
+          </Button>
+        </div>
 
         <div className="flex justify-center items-center">
           <div className="grid grid-cols-2 gap-8 p-2">
-            {!SELL_SUBSCRIPTIONS && (
-              <>
-                <div className="bg-white rounded-lg shadow p-6">
-                  <h2 className="font-semibold">Hobby</h2>
-                  <p className="text-gray-500">Great to start</p>
-                  <div className="my-4">
-                    <span className="text-4xl font-bold">$15</span>
-                  </div>
-                  <a
-                    href="https://buy.stripe.com/8wM6sre70gBW1nqaEE"
-                    target="_blank"
-                  >
-                    <button className="bg-black text-white rounded py-2 px-4 w-full text-sm">
-                      Purchase Credits
-                    </button>
-                  </a>
-                  <ul className="mt-4 space-y-2">
-                    <li className="flex items-center">
-                      <FaCheckCircle className="text-black mr-2" />
-                      100 credits
-                    </li>
-                    <li className="flex items-center">
-                      <FaCheckCircle className="text-black mr-2" />
-                      Email support
-                    </li>
-                  </ul>
-                </div>
+            <div className="bg-white rounded-lg shadow p-6">
+              <h2 className="font-semibold">Hobby</h2>
+              <p className="text-gray-500">Great to start</p>
+              <div className="my-4">
+                <span className="text-4xl font-bold">
+                  {paymentInterval === "monthly" ? "$15" : "$150"}
+                </span>
+                <span className="text-gray-500">
+                  {paymentInterval === "monthly" ? "/ month" : "/ year"}
+                </span>
+              </div>
 
-                <div className="bg-white rounded-lg shadow p-6">
-                  <h2 className="font-semibold">Pro</h2>
-                  <p className="text-gray-500">Higher limits</p>
-                  <div className="my-4">
-                    <span className="text-4xl font-bold">$40</span>
-                    {/* <span className="text-gray-500"> / month</span> */}
-                  </div>
-
-                  <a
-                    href="https://buy.stripe.com/dR69ED3sm85qgikcMN"
-                    target="_blank"
-                  >
-                    <button className="bg-black text-white rounded py-2 px-4 w-full text-sm">
-                      Purchase Credits
-                    </button>
-                  </a>
-
-                  <ul className="mt-4 space-y-2">
-                    <li className="flex items-center">
-                      <FaCheckCircle className="text-black mr-2" />
-                      300 credits
-                    </li>
-                    <li className="flex items-center">
-                      <FaCheckCircle className="text-black mr-2" />
-                      Email support
-                    </li>
-                  </ul>
-                </div>
-              </>
-            )}
-
-            {SELL_SUBSCRIPTIONS && (
-              <>
-                <div className="bg-white rounded-lg shadow p-6">
-                  <h2 className="font-semibold">Hobby</h2>
-                  <p className="text-gray-500">Great to start</p>
-                  <div className="my-4">
-                    <span className="text-4xl font-bold">
-                      {paymentInterval === "monthly" ? "$15" : "$150"}
-                    </span>
-                    <span className="text-gray-500">
-                      {paymentInterval === "monthly" ? "/ month" : "/ year"}
-                    </span>
-                  </div>
-
-                  <button
-                    className="bg-black text-white rounded py-2 px-4 w-full text-sm
+              <button
+                className="bg-black text-white rounded py-2 px-4 w-full text-sm
                     flex justify-center items-center gap-x-2"
-                    onClick={() =>
-                      checkout(
-                        paymentInterval === "monthly"
-                          ? "hobby_monthly"
-                          : "hobby_yearly"
-                      )
-                    }
-                  >
-                    Subscribe {isLoadingCheckout && <Spinner />}
-                  </button>
+                onClick={() =>
+                  checkout(
+                    paymentInterval === "monthly"
+                      ? "hobby_monthly"
+                      : "hobby_yearly"
+                  )
+                }
+              >
+                Subscribe {isLoadingCheckout && <Spinner />}
+              </button>
 
-                  <ul className="mt-4 space-y-2">
-                    <li className="flex items-center">
-                      <FaCheckCircle className="text-black mr-2" />
-                      100 credits / mo
-                    </li>
-                    <li className="flex items-center">
-                      <FaCheckCircle className="text-black mr-2" />
-                      OpenAI models
-                    </li>
-                    <li className="flex items-center">
-                      <FaCheckCircle className="text-black mr-2" />
-                      Claude models
-                    </li>
-                    <li className="flex items-center">
-                      <FaCheckCircle className="text-black mr-2" />
-                      Chat support
-                    </li>
-                  </ul>
-                </div>
+              <ul className="mt-4 space-y-2">
+                <li className="flex items-center">
+                  <FaCheckCircle className="text-black mr-2" />
+                  100 credits / mo
+                </li>
+                <li className="flex items-center">
+                  <FaCheckCircle className="text-black mr-2" />
+                  OpenAI models
+                </li>
+                <li className="flex items-center">
+                  <FaCheckCircle className="text-black mr-2" />
+                  Claude models
+                </li>
+                <li className="flex items-center">
+                  <FaCheckCircle className="text-black mr-2" />
+                  Chat support
+                </li>
+              </ul>
+            </div>
 
-                <div className="bg-white rounded-lg shadow p-6">
-                  <h2 className="font-semibold">Pro</h2>
-                  <p className="text-gray-500">Higher limits</p>
-                  <div className="my-4">
-                    <span className="text-4xl font-bold">
-                      {paymentInterval === "monthly" ? "$40" : "$400"}
-                    </span>
-                    <span className="text-gray-500">
-                      {paymentInterval === "monthly" ? "/ month" : "/ year"}
-                    </span>
-                  </div>
+            <div className="bg-white rounded-lg shadow p-6">
+              <h2 className="font-semibold">Pro</h2>
+              <p className="text-gray-500">Higher limits</p>
+              <div className="my-4">
+                <span className="text-4xl font-bold">
+                  {paymentInterval === "monthly" ? "$40" : "$400"}
+                </span>
+                <span className="text-gray-500">
+                  {paymentInterval === "monthly" ? "/ month" : "/ year"}
+                </span>
+              </div>
 
-                  <button
-                    className="bg-black text-white rounded py-2 px-4 w-full text-sm 
+              <button
+                className="bg-black text-white rounded py-2 px-4 w-full text-sm 
                       flex justify-center items-center gap-x-2"
-                    onClick={() =>
-                      checkout(
-                        paymentInterval === "monthly"
-                          ? "pro_monthly"
-                          : "pro_yearly"
-                      )
-                    }
-                  >
-                    Subscribe {isLoadingCheckout && <Spinner />}
-                  </button>
+                onClick={() =>
+                  checkout(
+                    paymentInterval === "monthly" ? "pro_monthly" : "pro_yearly"
+                  )
+                }
+              >
+                Subscribe {isLoadingCheckout && <Spinner />}
+              </button>
 
-                  <ul className="mt-4 space-y-2">
-                    <li className="flex items-center">
-                      <FaCheckCircle className="text-black mr-2" />
-                      300 credits / mo
-                    </li>
-                    <li className="flex items-center">
-                      <FaCheckCircle className="text-black mr-2" />
-                      OpenAI models
-                    </li>
-                    <li className="flex items-center">
-                      <FaCheckCircle className="text-black mr-2" />
-                      Claude models
-                    </li>
-                    <li className="flex items-center">
-                      <FaCheckCircle className="text-black mr-2" />
-                      Chat support
-                    </li>
-                  </ul>
-                </div>
-              </>
-            )}
+              <ul className="mt-4 space-y-2">
+                <li className="flex items-center">
+                  <FaCheckCircle className="text-black mr-2" />
+                  300 credits / mo
+                </li>
+                <li className="flex items-center">
+                  <FaCheckCircle className="text-black mr-2" />
+                  OpenAI models
+                </li>
+                <li className="flex items-center">
+                  <FaCheckCircle className="text-black mr-2" />
+                  Claude models
+                </li>
+                <li className="flex items-center">
+                  <FaCheckCircle className="text-black mr-2" />
+                  Chat support
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
         <p className="text-center text-xs text-gray-600 mt-1">
-          1 credit = 1 code generation.{" "}
-          {!SELL_SUBSCRIPTIONS && <>Unused credits expire after 90 days.</>}
-          {SELL_SUBSCRIPTIONS && (
-            <>
-              <br />
-              Cancel subscription at any time.
-            </>
-          )}
+          1 credit = 1 code generation. <br />
+          Cancel subscription at any time.
         </p>
 
         <DialogFooter></DialogFooter>
