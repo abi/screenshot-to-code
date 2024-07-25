88f4e81b96a8627ec02d5b1623365d1c8500e6b5
Abi Raja <abimanyuraja@gmail.com>
2024-07-25 14:59:13 -0400

    update comment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -354,7 +354,7 @@ function App() {
       )}
       <div className="lg:fixed lg:inset-y-0 lg:z-40 lg:flex lg:w-96 lg:flex-col">
         <div className="flex grow flex-col gap-y-2 overflow-y-auto border-r border-gray-200 bg-white px-6 dark:bg-zinc-950 dark:text-white">
-          {/* Header with access to settings*/}
+          {/* Header with access to settings */}
           <div className="flex items-center justify-between mt-10 mb-2">
             <h1 className="text-2xl ">Screenshot to Code</h1>
             <SettingsDialog settings={settings} setSettings={setSettings} />
