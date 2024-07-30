92f2933f0a920e4e6a5a022def17348a8b107ecf
Abi Raja <abimanyuraja@gmail.com>
2024-07-30 12:54:09 -0400

    update page path to not be nested

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -24,7 +24,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           <Route path="/" element={<AppContainer />} />
           <Route path="/evals" element={<EvalsPage />} />
           <Route path="/pricing" element={<PricingPage />} />
-          <Route path="/checkout/success" element={<CheckoutSuccessPage />} />
+          <Route path="/checkout-success" element={<CheckoutSuccessPage />} />
         </Routes>
       </Router>
       <Toaster
