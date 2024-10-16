752e6eb524f70d569d7b027abafdb54e5c06f399
Abi Raja <abimanyuraja@gmail.com>
2024-10-16 17:25:23 -0400

    re-enable sentry source maps

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,16 +3,16 @@ import { defineConfig, loadEnv } from "vite";
 import checker from "vite-plugin-checker";
 import react from "@vitejs/plugin-react";
 import { createHtmlPlugin } from "vite-plugin-html";
-// import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   return defineConfig({
     base: "",
-    // build: {
-    //   sourcemap: true,
-    // },
+    build: {
+      sourcemap: true,
+    },
     plugins: [
       react(),
       checker({ typescript: true }),
@@ -25,11 +25,11 @@ export default ({ mode }) => {
           },
         },
       }),
-      // sentryVitePlugin({
-      //   authToken: process.env.SENTRY_AUTH_TOKEN,
-      //   org: "whimsyworks",
-      //   project: "s2c-frontend",
-      // }),
+      sentryVitePlugin({
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+        org: "whimsyworks",
+        project: "s2c-frontend",
+      }),
     ],
     resolve: {
       alias: {
