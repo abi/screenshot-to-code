95ea3e24d15461fdf73414fa759bebcee368ee0d
Abi Raja <abimanyuraja@gmail.com>
2024-10-16 17:37:00 -0400

    update tracesSampleRate

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -21,12 +21,14 @@ Sentry.init({
     Sentry.replayIntegration(),
   ],
   // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  tracesSampleRate: 0.2,
+
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
   // tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+
   // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
