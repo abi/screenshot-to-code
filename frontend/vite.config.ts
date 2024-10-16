0494253fa806c54846de2536b4cf7dbde5990ad2
Abi Raja <abimanyuraja@gmail.com>
2024-10-16 13:45:00 -0400

    set up source map uploads during vite build process

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,12 +3,16 @@ import { defineConfig, loadEnv } from "vite";
 import checker from "vite-plugin-checker";
 import react from "@vitejs/plugin-react";
 import { createHtmlPlugin } from "vite-plugin-html";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   return defineConfig({
     base: "",
+    build: {
+      sourcemap: true,
+    },
     plugins: [
       react(),
       checker({ typescript: true }),
@@ -20,6 +24,11 @@ export default ({ mode }) => {
               : "",
           },
         },
+      }),
+      sentryVitePlugin({
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+        org: "whimsyworks",
+        project: "s2c-frontend",
       }),
     ],
     resolve: {
