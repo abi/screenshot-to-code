54e396eeaf58a255abcc927539d724cb75ffeb3f
Abi Raja <abimanyuraja@gmail.com>
2024-12-10 15:13:59 -0500

    add newer GPT4o models

--- backend/llm.py
+++ backend/llm.py
@@ -16,6 +16,8 @@ class Llm(Enum):
     GPT_4_VISION = "gpt-4-vision-preview"
     GPT_4_TURBO_2024_04_09 = "gpt-4-turbo-2024-04-09"
     GPT_4O_2024_05_13 = "gpt-4o-2024-05-13"
+    GPT_4O_2024_08_06 = "gpt-4o-2024-08-06"
+    GPT_4O_2024_11_20 = "gpt-4o-2024-11-20"
     CLAUDE_3_SONNET = "claude-3-sonnet-20240229"
     CLAUDE_3_OPUS = "claude-3-opus-20240229"
     CLAUDE_3_HAIKU = "claude-3-haiku-20240307"
