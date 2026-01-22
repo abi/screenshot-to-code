0b46806a86f418224ba7b7f5b2a0338db22b6951
Abi Raja <abimanyuraja@gmail.com>
2026-01-22 14:41:35 -0500

    Remove animation from '10x Faster'

--- frontend/src/components/hosted/LandingPage.tsx
+++ frontend/src/components/hosted/LandingPage.tsx
@@ -128,7 +128,7 @@ function LandingPage() {
                 <br />
                 <span className="font-editorial not-italic">Interfaces</span>
                 <br />
-                <span className="text-glow-pulse">10x Faster</span>
+                10x Faster
               </h1>
 
               {/* Subheadline */}
