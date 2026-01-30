cbbf8557bf98f861fb85a03fcabc8fcaf24dfb18
Abi Raja <abimanyuraja@gmail.com>
2026-01-30 16:12:32 -0500

    Fix node imports in QA test

--- frontend/src/tests/qa.test.ts
+++ frontend/src/tests/qa.test.ts
@@ -1,5 +1,5 @@
-import fs from "fs";
-import path from "path";
+import * as fs from "fs";
+import * as path from "path";
 import puppeteer, { Browser, ElementHandle, Page } from "puppeteer";
 import { Stack } from "../lib/stacks";
 import { CodeGenerationModel } from "../lib/models";
